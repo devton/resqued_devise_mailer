f4e735936a145486cc9e8cebb683249f809cd5c3
BJK <bernie@thecoderie.com>
2011-09-15 14:00:37 -0400

    updated to work with devise ~ 1.4.5

--- config/initializers/resqued_devise_mailer.rb
+++ config/initializers/resqued_devise_mailer.rb
@@ -1,37 +1,21 @@
-class ResquedDeviseMailer < Devise::Mailer
-  include Resque::Mailer
+module Devise
+  module Mailers
+    module Helpers
+      def initialize_from_record(record)
+        record = hack_record(record)
+        @scope_name = Devise::Mapping.find_scope!(record)
+        @resource   = instance_variable_set("@#{devise_mapping.name}", record)
+      end
+      
+      protected
+      # monkey patch :D
+      def hack_record(record)
+        record.kind_of?(Hash) ? kmodel(record).where(:email=>record.first[1]['email']).first : record
+      end
 
-  private
-  # Configure default email options
-  def setup_mail(record, action)
-
-    record = hack_record(record)
-
-    @scope_name     = Devise::Mapping.find_scope!(record)
-    @devise_mapping = Devise.mappings[@scope_name]
-    @resource       = instance_variable_set("@#{@devise_mapping.name}", record)
-
-    headers = {
-      :subject => translate(@devise_mapping, action),
-      :from => mailer_sender(@devise_mapping),
-      :to => record.email,
-      :template_path => template_paths,
-    }
-
-    headers.merge!(record.headers_for(action)) if record.respond_to?(:headers_for)
-    mail(headers) do |format|
-      format.html { render "app/views/devise/mailer/#{action}" }
+      def kmodel(record)
+        record.first[0].camelize.constantize
+      end
     end
   end
-
-  protected
-  # monkey patch :D
-  def hack_record(record)
-    record.kind_of?(Hash) ? kmodel(record).find(record.first[1]['id']) : record
-  end
-
-  def kmodel(record)
-    record.first[0].camelize.constantize
-  end
-
 end
